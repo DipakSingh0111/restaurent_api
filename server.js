911250b4719db4c984ba74657d20f5c0d580b97f
Dipak Singh <dipaksingh3323@gmail.com>
2025-02-15 11:14:18 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+const app = express();
+// dotenv config call
+dotenv.config();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+app.get('/', (req,res)=>{
+    res.send("Getting all Product using get Method...");
+});
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, ()=>{
+    console.log(`Server Running on PORT ${PORT}`)
+});
